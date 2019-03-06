607201dd4efdde249be300a320da03af3d769899
Mohammad Hazara <mohammadvhazara@gmail.com>
2019-03-06 12:18:12 +0100

    Replaced Project

---
+++ DTUBankingSystem/DtuNetbank/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DtuNetbank.Models
+{
+    public class Account
+    {
+        public long AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string AccountId { get; set; }
+    }
+}
---
+++ DTUBankingSystem/DtuNetbank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DtuNetbank.Startup))]
+namespace DtuNetbank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
