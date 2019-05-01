8b0c48e23d606d27ab508029b77384b5c8be1d26
Jimmy <tippy_1990@hotmail.com>
2019-05-01 04:13:23 +0200

    You can only see the Bank accounts that you've registrered.  the new bank accounts are missing GUIDs

--- DTUBankingSystem/DtuNetbank/Controllers/Netbank/BankAccountController.cs
+++ DTUBankingSystem/DtuNetbank/Controllers/Netbank/BankAccountController.cs
@@ -12,17 +12,58 @@ namespace DtuNetbank.Controllers.Netbank
     public class BankAccountController : PortalController
     {
         // GET: BankAccount
-        public ActionResult Index()
+        public ActionResult Index(AccountMessageId? Message)
         {
             var user = GetCurrentUser();
-            var accountJsonModels = GetAccounts();
-            var accounts = accountJsonModels.Select(a => new BankAccount()
-            {
-                AccountName = a.account_name, IBAN = a._id,
-                AccountNumber = a.account_numbers.Single(i => i._type == "BBAN_DK").value, User = user,
-                Balance = decimal.Parse(a.available_balance)
-            }).ToList();
+            //var accountJsonModels = GetAccounts();            
+            //var accounts = accountJsonModels.Select(a => new BankAccount()
+            //{
+            //    AccountName = a.account_name, IBAN = a._id,
+            //    AccountNumber = a.account_numbers.Single(i => i._type == "BBAN_DK").value, User = user,
+            //    Balance = decimal.Parse(a.available_balance)
+            //}).ToList();
+
+            var accounts = GetUserAccounts();
+            string status = Message == AccountMessageId.AccountAlreadyAdded? "Denne konto er allerede registreret" :
+                            Message == AccountMessageId.UnableToFindAccount? "Denne konto eksistere ikke" :
+                            Message == AccountMessageId.AccountSuccesfullyAdded? "Konto tilføjet!" : "";
+            ViewBag.statusMessage = status;
             return View("Accounts", accounts);
+        }
+        public ActionResult AddAccount(string accountId)
+        {
+            if(accountId == null)
+                RedirectToAction("Index", "BankAccount");
+
+            var userid = User.Identity.GetUserId();
+            AccountMessageId responseMsg;
+            using (var db = new ApplicationDbContext())
+            {
+                var accounts  = db.BankAccounts.Where(a => a.UserId.ToString() == userid 
+                                                        && a.AccountNumber.ToString() == accountId).ToList();
+                if(accounts.Count > 0)
+                    responseMsg = AccountMessageId.AccountAlreadyAdded;
+                else{
+                    var accountJsonModels = GetAccounts();
+                    var accountsFromNordea = accountJsonModels.
+                                            Where(a => a._id.ToString() == accountId).
+                                            Select(a => new BankAccount()
+                                            {
+                                                AccountName = a.account_name, IBAN = a._id,
+                                                AccountNumber = a.account_numbers.Single(i => i._type == "BBAN_DK").value,
+                                                User = GetCurrentUser(), Balance = decimal.Parse(a.available_balance),
+                                            }).
+                                            ToList();
+                    if(accountsFromNordea.Count == 0)
+                        responseMsg = AccountMessageId.UnableToFindAccount;
+                    else{
+                        db.Set<BankAccount>().Add(accountsFromNordea.ElementAt(0));
+                        db.SaveChanges();
+                        responseMsg = AccountMessageId.AccountSuccesfullyAdded;
+                    }
+                }
+            }
+            return RedirectToAction("Index", "BankAccount" , new { Message = responseMsg });
         }
         
 
@@ -56,6 +97,13 @@ namespace DtuNetbank.Controllers.Netbank
             
             var model = new TransactionViewModel(accountId, jsonTransactions, user);
             return View(model);
+        }
+
+        public enum AccountMessageId
+        {
+            AccountSuccesfullyAdded,
+            UnableToFindAccount,
+            AccountAlreadyAdded
         }
 
     }
